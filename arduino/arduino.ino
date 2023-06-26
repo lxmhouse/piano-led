c26bd6842699fce58efc79ef63e75766f975713b
Raffi Hotter <rhotter@users.noreply.github.com>
2023-06-26 00:36:30 -0400

    initial commit

---
+++ arduino/arduino.ino
@@ -0,0 +1,142 @@
+#include <Adafruit_NeoPixel.h>
+
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define PIN 6
+#define NUM_PIXELS 24
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRBW + NEO_KHZ800);
+
+int parseData(String data, int *intList)
+{
+  char *chrArray = strdup(data.c_str());
+  int i = 0;
+
+  char *token = strtok(chrArray, ",");
+  while (token != NULL)
+  {
+    intList[i] = atoi(token);
+    i++;
+    token = strtok(NULL, ",");
+  }
+
+  free(chrArray);
+  return i; // Return size of filled data in intList
+}
+
+void setup()
+{
+#if defined(__AVR_ATtiny85__)
+  if (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
+#endif
+
+  strip.begin();
+  strip.setBrightness(20);
+  strip.show();
+
+  // Begin serial communication.
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+  // colorWipe(strip.Color(255, 0, 0), 1); // Red
+  // black();
+  white();
+}
+
+void loop()
+{
+  // Check if data is available to read.
+  if (Serial.available() > 0)
+  {
+    // Read the data in and convert it to an integer.
+    String notes = Serial.readStringUntil('\n');
+
+    // int note = Serial.readString().toInt();
+    black();
+
+    int noteVec[50]; // Manually managed array. You might need to adjust the size.
+    int size = parseData(notes, noteVec);
+    for (int i = 0; i < size; i++)
+    {
+      int idx = noteVec[i] % NUM_PIXELS;
+      strip.setPixelColor(idx, random(0, 255), random(0, 255), random(0, 255), 0);
+    }
+
+    // // Calculate the octave based on the MIDI note number.
+    // int octave = note / 12;
+
+    // // Modulate the brightness based on the octave.
+    // // Here, we just set it to 10 times the octave number, but you might want to adjust this calculation.
+    // int brightness = octave * 10;
+    // brightness = constrain(brightness, 0, 255); // Ensure brightness stays within valid range.
+
+    // strip.setPixelColor(idx, 0, 0, 0, 255);
+
+    // Set the brightness.
+    // strip.setBrightness(brightness);
+    // colorWipe(strip.Color(0, 0, 255), brightness); // Red
+
+    // Show the changes on the strip.
+    strip.show();
+  }
+}
+
+void white()
+{
+  for (uint16_t i = 0; i < strip.numPixels(); i++)
+  {
+    strip.setPixelColor(i, 0, 0, 0, 255);
+  }
+  strip.show();
+}
+
+void black()
+{
+  for (uint16_t i = 0; i < strip.numPixels(); i++)
+  {
+    strip.setPixelColor(i, 0, 0, 0);
+  }
+  strip.show();
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait)
+{
+  for (uint16_t i = 0; i < strip.numPixels(); i++)
+  {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+
+  // for (uint16_t i = 0; i < strip.numPixels(); i++)
+  // {
+  //   strip.setPixelColor(i, 10, 10, 10);
+  // }
+
+  strip.show();
+}
+
+void theaterChase(uint32_t c, uint8_t wait)
+{
+  for (int j = 0; j < 10; j++)
+  {
+    for (int q = 0; q < 3; q++)
+    {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3)
+      {
+        strip.setPixelColor(i + q, c);
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3)
+      {
+        strip.setPixelColor(i + q, 0);
+      }
+    }
+  }
+}
---
+++ main.py
@@ -0,0 +1,49 @@
+import mido
+import serial
+import time
+
+
+def stream(inport):
+    print("Reading MIDI messages. Press Ctrl+C to stop.")
+    current_notes = set()
+    pitch_bend = 0
+    for msg in inport:
+        if msg.type == "clock":
+            continue
+        elif msg.type == "note_on":
+            if msg.velocity > 0:
+                current_notes.add(msg.note)
+                send_notes(current_notes)
+            elif msg.note in current_notes:
+                current_notes.remove(msg.note)
+        elif msg.type == "pitchwheel":
+            pitch_bend = msg.pitch
+        if len(current_notes) != 0:
+            print(current_notes, "pitch: ", pitch_bend)
+
+
+def send_notes(notes):
+    """
+    Send notes to Arduino via serial with throttling.
+    """
+    print("Sending")
+    note = ",".join(str(n) for n in notes) + "\n"
+    note_bytes = str(note).encode("utf-8")
+    arduino_serial.write(note_bytes)
+
+
+arduino_serial = serial.Serial("/dev/cu.usbserial-1440", 9600)
+
+ports = mido.get_input_names()
+print("All available MIDI ports:")
+for port in ports:
+    print(port)
+
+yamaha_port = next((port for port in ports if "YAMAHA" in port), None)
+if yamaha_port is None:
+    print("Yamaha piano not found.")
+else:
+    print(f"Yamaha piano found: {yamaha_port}")
+
+    with mido.open_input(yamaha_port) as inport:
+        stream(inport)
